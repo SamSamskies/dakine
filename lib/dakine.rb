df8150610f16ebc9f959ed4bb60cb491cd037b4f
Steven Miyakawa SAM <samprofessional@gmail.com>
2014-06-08 17:40:35 -0700

    Refactor everything

--- lib/dakine.rb
+++ lib/dakine.rb
@@ -8,15 +8,14 @@ module Dakine
     tagged_word_pairs = tag_words(text)
 
     translated_text = tagged_word_pairs.map do |tw_pair|
-
-                        if is_noun?(tw_pair)
-                          tw_pair[:word] = 'da kine'
-                        elsif tw_pair[:word] == 'the'
-                          tw_pair[:word] = 'da'
-                        end
-
-                        tw_pair[:word]
-                      end.join(' ')
+      if is_noun?(tw_pair[:tag])
+        replace_noun
+      elsif tw_pair[:word] == 'the'
+        'da'
+      else
+        tw_pair[:word]
+      end
+    end.join(' ')
 
     remove_whitespace_before_punctuation translated_text
   end
@@ -27,14 +26,18 @@ module Dakine
     tagged_word_pairs.map { |tw_pair| { word: tw_pair.first, tag: tw_pair.last }}
   end
 
-  def is_noun?(tw_pair)
-    return true if tw_pair[:tag] == "NN" ||
-                   tw_pair[:tag] == "NNP" ||
-                   tw_pair[:tag] == "NNPS" ||
-                   tw_pair[:tag] == "NNS"
+  def is_noun?(tag)
+    return true if tag == "NN" ||
+                   tag == "NNP" ||
+                   tag == "NNPS" ||
+                   tag == "NNS"
+  end
+
+  def replace_noun
+    'da kine'
   end
 
   def remove_whitespace_before_punctuation(text)
-    text.gsub /\s+([.,;?!])/, '\1'
+    text.gsub /\s+([.,;?!'])/, '\1'
   end
 end
